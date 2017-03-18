fbbd3f732757e9462f55791d9bee632c0665e9ac
natashabibbs <nbibbs2411@myhta.org>
2017-03-17 14:14:01 -1000

    first commit

--- app.js
+++ app.js
@@ -9,65 +9,71 @@
 */
 
 // Addition (+)
-5 + 9;
+5 + 9; //14
 
-"one" + "one";
+console.log("one" + "one");
 
 // Subtraction (-)
-8 - 20;
+console.log(8 - 20); //-12
 
 // Multiplication (*)
-2 * 0;
+console.log(2 * 0);//0
 
 // Division (/)
-15/50;
+console.log(15/50);//0.3
 
 // Modulus (%)
-5 % 2;
+console.log(5 % 2);//1
 
 // Increment (++)
-2++;
+ var a = 2;
+a++;
+console.log(a);
 
--1++;
-
+//-1++;//this one is invalid as well jesus christ
+ var b = -1;
+ b++;
+ console.log("Increment -1", b);
 // Decrement (--)
-5--;
-
+//5--;//this is invalid OMG so many invalids
+var c = 5;
+c++;
+console.log("Increment 5", c)
 /*
 * Comparison Operators (These return a boolean value of true or false)
 * For those with the boolean answer false, change the expression to evaluate to true
 */
 
 // Strict Equal (===)
-5 === "five";
+console.log(5 === "five");// this one's invalid too whyyyy
 
 // Not Equal (!==)
-5!== "five";
+console.log(5!== "five");// yayyy a true one for once
 
 // Greater Than (>)
-5 > 10;
+console.log(5 > 10);//dangit another false
 
 // Less Than (<)
-"20" < 20;
+console.log("20" < 20);// so many false much wow
 
 // Greater than or Equal to (>=)
-undefined >= null;
+console.log(undefined >= null);//false again
 
 // Less than or Equal to (<=)
-null <= null; 
+console.log(null <= null); //trueeeeee yaaa boiii
 
 /*
 * Logical (or Relational) Operators
 */
 
 // And (&&) - If both the operands satisfy the statment, then the condition becomes true
-1 > 0 && 1 < 0;
+console.log(1 > 0 && 1 < 0);//true
 
 // Or (||) - If any of the two operands satisfy the statement, then the condition becomes true.
-2 > 0 || 2 > 20;
+console.log(2 > 0 || 2 > 20);//trueeee
 
 // Not (!) - Reverses the logical state of its operand.
-!true;
+console.log(!true);//false
 
 /*
 * Conditionals
@@ -75,3 +81,7 @@ null <= null;
 */
 var a = "apple";
 var b = 2;
+console.log("");}
+else {
+	console.log("")
+}
